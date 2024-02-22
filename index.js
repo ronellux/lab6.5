b8900f47b12266d261bf81bf4f24a8152e26a57d
ronellux <156017041+ronellux@users.noreply.github.com>
2024-02-22 03:07:34 +0000

    latest commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Add JavaScript below
+ScrollReveal().reveal('.pic1',{delay: 1000});
+ScrollReveal().reveal('.pic2',{duration: 600});
+ScrollReveal().reveal('.pic3',{distance: '0px'});
+ScrollReveal().reveal('.pic4',{interval: 0});
+ScrollReveal().reveal('.pic5',{origin: 'bottom'});
