0248205b11db4492b049ae970108690179b0a9ff
ronellux <156017041+ronellux@users.noreply.github.com>
2024-02-26 23:12:27 +0000

    newest commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-ScrollReveal().reveal('.pic1',{delay: 1000});
-ScrollReveal().reveal('.pic2',{duration: 600});
+ScrollReveal().reveal('.pic1',{duration: 600});
+ScrollReveal().reveal('.pic2',{delay: 600});
 ScrollReveal().reveal('.pic3',{distance: '0px'});
 ScrollReveal().reveal('.pic4',{interval: 0});
 ScrollReveal().reveal('.pic5',{origin: 'bottom'});
